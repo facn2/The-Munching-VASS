5a728d0bddd86162c1d08c66b8e0f98f21d5b2b0
Wyrd00 <amy.tai0120@gmail.com>
2017-08-08 16:56:01 +0300

    create files relates #1

---
+++ src/router.js
@@ -0,0 +1,31 @@
+const handler = require('handler');
+const pg = require('pg');
+const fs = require('fs');
+const querystring = require('querystring');
+const data = require('./queries/data.js')
+
+
+const router = (request, response) => {
+	const url = request.url.split('/')[1];
+
+	if (url === ''){
+		handler.handleHome 
+	} else if (url === 'public') {
+		handler.handlePublic
+	} else if (url === 'calendar') {
+		handler.getData
+	} else if (url === 'addchef') {
+	 	handler.addChef
+	} else if (url === 'addsouschef') {
+	 	handler.addSousChef
+	} else if (url === 'addmeal') {
+	 	handler.addMeal
+	} else if (url === 'addbudget') {
+	 	handler.addBudget
+	} else {
+
+	}
+
+
+
+ module.exports = router;
