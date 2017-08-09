2a316731efdfe5711baed3cb4f63e94164e198e6
Wyrd00 <amy.tai0120@gmail.com>
2017-08-09 12:58:04 +0300

    DOM for response data relates #21'

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 var form = document.querySelector("#form");
+var firstRow = document.querySelector("#firstRow")
 
 var makeRequest = function(url, callback) {
 	var dbCall = new XMLHttpRequest();
@@ -13,12 +14,39 @@ var makeRequest = function(url, callback) {
     dbCall.send();
 }
 
-makeRequest('/calendar', callback//replace later);
 
-form.addEventListener('submit', function(event) {
-	event.preventDefault();
-	
-	
+var updateDom = function(err, data) {
+	if (err) {
+		console.log('Yikes error again blah')
+	}
+	const dataObj = JSON.parse(data)
+	console.log(dataObj)
 
-	makeRequest('updateCalendar', callback)
-});
+	dataObj.forEach(function(dataObj) {
+		var date = dataObj.date
+		var cellDate = firstRow.cells[0]
+		cellDate.innerHTML = date
+
+		var chef = dataObj.chef
+		var cellChef = firstRow.cells[1]
+		cellChef.innerHTML = chef
+
+		var sousChef = dataObj.sousChef
+		var cellSousChef = firstRow.cells[2]
+		cellSousChef.innerHTML = sousChef
+
+		var meal = dataObj.meal
+		var cellMeal = firstRow.cells[3]
+		cellMeal.innerHTML = meal
+
+		var budget = dataObj.budget
+		var cellBudget = firstRow.cells[4]
+		cellBudget.innerHTML = budget
+
+		//for attending, assuming that list of people within their own object
+		// var attending = dataObj.attending
+		// var cellAttending = firstRow.cells[5]
+	})
+}
+
+makeRequest('/calender', updateDom)
