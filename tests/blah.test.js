3280f2ef9ba3d056ac42e1b225ce98e6d540b95e
Wyrd00 <amy.tai0120@gmail.com>
2017-08-09 15:40:27 +0300

    started dummy test relates #30

---
+++ tests/blah.test.js
@@ -0,0 +1,10 @@
+tape('Zero - Working I am?', (t) => {
+	let num1 20;
+	t.deepequal = (20, num, "20, 20 - should be perfect vision")
+	t.end();
+} 
+
+// tape('First - Check if status code is 200', (t) => {
+// 	shot(router, {method: 'GET', url: '/'}, )
+// 	t.deepEqual = ()
+// })
