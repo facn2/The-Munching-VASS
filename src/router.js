83777341d85327d1782effd623e5adb3d360d694
Vered Rekanati <veredrec@gmail.com>
2017-08-09 15:29:19 +0300

    fix getData function Relates #28

--- src/router.js
+++ src/router.js
@@ -5,26 +5,15 @@ const router = (request, response) => {
 	const url = request.url.split('/')[1];
 
 	if (url === ''){
-		handler.handleHome(response);
+		handler.handleHome(request, response);
 	} else if (url === 'public') {
 		handler.handlePublic(request, response)
 	} else if (url === 'get-data') {
-		handler.getData();
+		handler.getData(request, response);
 	} else if (url === '/update-data') {
-		handler.updateTable();
+		handler.updateTable(request, response);
 	}
 
-	// else if (url === 'addchef') {
-	//  	handler.addChef
-	// } else if (url === 'addsouschef') {
-	//  	handler.addSousChef
-	// } else if (url === 'addmeal') {
-	//  	handler.addMeal
-	// } else if (url === 'addbudget') {
-	//  	handler.addBudget
-	else {
-
-	}
 }
 
 
